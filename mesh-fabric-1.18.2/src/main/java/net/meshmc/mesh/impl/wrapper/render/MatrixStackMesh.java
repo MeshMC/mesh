237d08dbb550599ef8d2a2dc9b02282919bf7e75
Tigermouthbear <tigermouthbear@tigr.dev>
2022-06-30 00:23:50 -0400

    add matrix getters

--- mesh-fabric-1.18.2/src/main/java/net/meshmc/mesh/impl/wrapper/render/MatrixStackMesh.java
+++ mesh-fabric-1.18.2/src/main/java/net/meshmc/mesh/impl/wrapper/render/MatrixStackMesh.java
@@ -1,7 +1,11 @@
 package net.meshmc.mesh.impl.wrapper.render;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import net.meshmc.mesh.api.render.buffer.MatrixStack;
 import net.minecraft.util.math.Quaternion;
+import org.lwjgl.BufferUtils;
+
+import java.nio.FloatBuffer;
 
 /**
  * @author Tigermouthbear 1/15/22
@@ -32,6 +36,20 @@ public class MatrixStackMesh implements MatrixStack {
     @Override
     public void rotate(float angle, float x, float y, float z) {
         matrixStack.multiply(new Quaternion(x * angle, y * angle, z * angle, true));
+    }
+
+    @Override
+    public FloatBuffer getProjectionMatrix() {
+        FloatBuffer buffer = BufferUtils.createFloatBuffer(16);
+        RenderSystem.getProjectionMatrix().write(buffer, false);
+        return buffer;
+    }
+
+    @Override
+    public FloatBuffer getModelMatrix() {
+        FloatBuffer buffer = BufferUtils.createFloatBuffer(16);
+        matrixStack.peek().getPositionMatrix().write(buffer, false);
+        return buffer;
     }
 
     public void setMatrixStack(net.minecraft.client.util.math.MatrixStack matrixStack) {
--- mesh-forge-1.12.2/src/main/java/net/meshmc/mesh/impl/wrapper/render/MatrixStackMesh.java
+++ mesh-forge-1.12.2/src/main/java/net/meshmc/mesh/impl/wrapper/render/MatrixStackMesh.java
@@ -2,6 +2,10 @@ package net.meshmc.mesh.impl.wrapper.render;
 
 import net.meshmc.mesh.api.render.buffer.MatrixStack;
 import net.minecraft.client.renderer.GlStateManager;
+import org.lwjgl.BufferUtils;
+import org.lwjgl.opengl.GL11;
+
+import java.nio.FloatBuffer;
 
 /**
  * @author Tigermouthbear 1/15/22
@@ -30,5 +34,21 @@ public class MatrixStackMesh implements MatrixStack {
     @Override
     public void rotate(float angle, float x, float y, float z) {
         GlStateManager.rotate(angle, x, y, z);
+    }
+
+    @Override
+    public FloatBuffer getProjectionMatrix() {
+        return getMatrix(GL11.GL_PROJECTION_MATRIX);
+    }
+
+    @Override
+    public FloatBuffer getModelMatrix() {
+        return getMatrix(GL11.GL_MODELVIEW_MATRIX);
+    }
+
+    private FloatBuffer getMatrix(int mode) {
+        FloatBuffer buffer = BufferUtils.createFloatBuffer(16);
+        GL11.glGetFloat(mode, buffer);
+        return buffer;
     }
 }
