88cff327504b424ae7b6ffdf31b575d876bbaacb
Tigermouthbear <tigermouthbear@tigr.dev>
2022-07-17 22:39:11 -0400

    enable stencil in 1.18

--- mesh-fabric-1.18.2/src/main/java/net/meshmc/mesh/impl/mixin/render/MixinFramebuffer.java
+++ mesh-fabric-1.18.2/src/main/java/net/meshmc/mesh/impl/mixin/render/MixinFramebuffer.java
@@ -1,9 +1,14 @@
 package net.meshmc.mesh.impl.mixin.render;
 
 import net.minecraft.client.gl.Framebuffer;
+import org.lwjgl.opengl.GL30;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.Slice;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 @Mixin(Framebuffer.class)
 public abstract class MixinFramebuffer implements net.meshmc.mesh.api.render.buffer.Framebuffer {
@@ -127,5 +132,21 @@ public abstract class MixinFramebuffer implements net.meshmc.mesh.api.render.buf
     @Override
     public void clear() {
         this.clear(false);
+    }
+
+    // enable stencil testing
+    @ModifyArgs(method = "initFbo", at = @At(value = "INVOKE",
+            target = "Lcom/mojang/blaze3d/platform/GlStateManager;_texImage2D(IIIIIIIILjava/nio/IntBuffer;)V", ordinal = 0))
+    public void stencil(Args args) {
+        args.set(2, GL30.GL_DEPTH32F_STENCIL8);
+        args.set(6, GL30.GL_DEPTH_STENCIL);
+        args.set(7, GL30.GL_FLOAT_32_UNSIGNED_INT_24_8_REV);
+    }
+
+    @ModifyArgs(method = "initFbo",
+            at = @At (value = "INVOKE", target = "Lcom/mojang/blaze3d/platform/GlStateManager;_glFramebufferTexture2D(IIIII)V"),
+            slice = @Slice(from = @At (value = "FIELD", target = "Lnet/minecraft/client/gl/Framebuffer;useDepthAttachment:Z", ordinal = 1)))
+    public void stencil1(Args args) {
+        args.set(1, GL30.GL_DEPTH_STENCIL_ATTACHMENT);
     }
 }
